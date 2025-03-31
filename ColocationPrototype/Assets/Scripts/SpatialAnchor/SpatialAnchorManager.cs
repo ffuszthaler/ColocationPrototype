5541d1b57c3ba3fc71f79142665b8ab876b22164
ffuszthaler <flo.fuszthaler@gmail.com>
2025-03-31 10:58:07 +0200

    feat: added scripts for spatial anchors & UUIDs

---
+++ ColocationPrototype/Assets/Scripts/SpatialAnchor/SpatialAnchorManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class SpatialAnchorManager : MonoBehaviour
+{
+    private Guid anchorGroupID;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anchorGroupID = Guid.NewGuid();
+        StartCoroutine(CreateSpacialAnchor());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator CreateSpacialAnchor()
+    {
+        Debug.Log("Creating spacial anchor");
+        
+        var anchor = gameObject.AddComponent<OVRSpatialAnchor>();
+        yield return new WaitUntil(() => anchor.Created);
+        
+        Debug.Log("Created Anchor: " + anchor.Uuid.ToString());
+
+        var saveResult =   anchor.SaveAnchorAsync();
+        yield return new WaitUntil(() => saveResult.IsCompleted);
+        
+        var saveRes = saveResult.GetResult();
+
+        if (saveRes.Success)
+        {
+            Debug.Log("Saved spacial anchor: " + saveRes.Status.ToString());
+        }
+        else
+        {
+            Debug.Log("Failed to save spacial anchor: " + saveRes.Status.ToString());
+        }
+
+        var shareResult = anchor.ShareAsync(anchorGroupID);
+        yield return new WaitUntil(() => shareResult.IsCompleted);
+        
+        var shareRes = shareResult.GetResult();
+        
+        if (shareRes.Success)
+        {
+            Debug.Log("Shared spacial anchor: " + shareRes.Status.ToString());
+        }
+        else
+        {
+            Debug.Log("Failed to share spacial anchor: " + shareRes.Status.ToString());
+        }
+    }
+}
---
+++ ColocationPrototype/Assets/Scripts/UUID/RpcCommunication.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class RpcCommunication : NetworkBehaviour
+{
+    public override void OnNetworkSpawn()
+    {
+        if (!IsServer && IsOwner)
+        {
+            ServerOnlyRpc(0, NetworkObjectId);
+        }
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    void ClientAndHostRpc(int value, ulong sourceNetworkObjectId)
+    {
+        Debug.Log($"Client Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
+        if (IsOwner) //Only send an RPC to the owner of the NetworkObject
+        {
+            ServerOnlyRpc(value + 1, sourceNetworkObjectId);
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    void ServerOnlyRpc(int value, ulong sourceNetworkObjectId)
+    {
+        Debug.Log($"Server Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
+        ClientAndHostRpc(value, sourceNetworkObjectId);
+    }
+}
