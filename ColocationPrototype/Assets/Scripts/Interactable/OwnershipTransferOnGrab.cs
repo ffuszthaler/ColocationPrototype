cc3381a8b5a4575424a653236249d8d33daa27fa
ffuszthaler <flo.fuszthaler@gmail.com>
2025-04-24 18:07:04 +0200

    feat: client - server interactable seems to work

--- ColocationPrototype/Assets/Scripts/Interactable/OwnershipTransferOnGrab.cs
+++ ColocationPrototype/Assets/Scripts/Interactable/OwnershipTransferOnGrab.cs
@@ -8,36 +8,49 @@ using Oculus.Interaction.HandGrab;
 public class OwnershipTransferOnGrab : MonoBehaviour
 {
     // private Grabbable grabbable;
-    private HandGrabInteractable handGrabInteractable;
+    [SerializeField] private HandGrabInteractable handGrabInteractable;
 
     private void Awake()
     {
         // grabbable = GetComponent<Grabbable>();
         // grabbable.WhenSelect += OnGrabbed;
-        handGrabInteractable = gameObject.GetComponent<HandGrabInteractable>();
+        // handGrabInteractable = gameObject.GetComponent<HandGrabInteractable>();
     }
 
     private void Update()
     {
-        var hand = handGrabInteractable.Interactors.FirstOrDefault<HandGrabInteractor>();
-        if (hand != null)
-        {
-            Debug.Log("Connected to hand " + hand.gameObject.tag);
-            OnGrabbed();
-        }
+        // var hand = handGrabInteractable.Interactors.FirstOrDefault<HandGrabInteractor>();
+        // if (hand != null)
+        // {
+        //     Debug.Log("Connected to hand " + hand.gameObject.tag);
+        //     OnGrabbed();
+        // }
     }
 
-    private void OnGrabbed()
+    public void OnGrabbed()
     {
         if (NetworkManager.Singleton.IsServer)
         {
             // Transfer ownership to the client who grabbed the cube
             NetworkObject networkObject = GetComponent<NetworkObject>();
+            if (networkObject != null)
+            {
+                Debug.Log("Network Object found: " + networkObject.name);
+            }
+            else
+            {
+                Debug.Log("Network Object not found!");
+            }
             
             // ulong clientId = grabbable.SelectingInteractor.GetComponent<NetworkObject>().OwnerClientId;
-            
-            ulong clientId = handGrabInteractable.GetComponent<NetworkObject>().OwnerClientId;
+            ulong clientId = networkObject.OwnerClientId;
+            if (clientId != null)
+            {
+                Debug.Log("ClientID found: " + clientId);
+            }
             networkObject.ChangeOwnership(clientId);
+            
+            Debug.Log("Is Owner: " + networkObject.IsOwner);
         }
     }
 }
